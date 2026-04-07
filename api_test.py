e8d13482bc1d1f43e5db567cd7febf2dd665617b
c29mm <maxwellmassiah2.0@gmail.com>
2026-04-07 16:20:27 -0400

    Initialize Spotify client with authentication

--- api_test.py
+++ api_test.py
@@ -1,1 +1,9 @@
-
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        client_id="4a2e25ccb49d4a06be2008874f09a91a",
+        client_secret="1e6faae316a54646937b5272da7460c8",
+        redirect_uri="http://127.0.0.1:8888/callback",
+        scope="user-read-private",
+        open_browser=True
+    )
+)
